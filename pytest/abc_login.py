23266e4e91fdcf2f60464dcd8d5f9d2689d02ccf
gaomingduo <gmdjob@163.com>
2021-03-16 23:01:22 +0800

    test 2

---
+++ pytest/abc_login.py
@@ -0,0 +1,9 @@
+def test_login():
+    print("login")
+
+class LoginCases:
+    def case_a(self):
+        print("logCases === case_a")
+
+    def test_a(self):
+        print("LoginCases === test_a")
