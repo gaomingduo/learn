17b91e2baf2fe92ea4488b4d4b86966b1d2a6148
高明铎 <gmdjob@163.com>
2021-05-04 15:41:59 +0800

    no message

--- mitmproxy/mitmproxy_response.py
+++ mitmproxy/mitmproxy_response.py
@@ -5,5 +5,4 @@ def response(flow):
         data = json.loads(flow.response.content)
         data['data']['lucky_history'][0]['created_at'] = "2021-03-25 22:11:00"
         data['data']['lucky_history'][0]['lucky'] = -500
-
         flow.response.text = json.dumps(data)
